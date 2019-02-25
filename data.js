a1102fac51b1b51580053c253db39a6aee148eb9
Anand Upender <anand.upender@gmail.com>
2019-02-24 17:55:46 -0800

    got all images working

---
+++ data.js
@@ -0,0 +1,137 @@
+var data = [
+ {
+   "Image": "brownpaperbag.png",
+   "bin": "compost",
+   "incorrect": "If there is no food in it, put it in the paper bin! Otherwise, compost is your best bet. "
+ },
+ {
+   "Image": "bubblewrap.png",
+   "bin": "paper",
+   "incorrect": "This one is tricky. Clear plastics like cling and bubble wrap are sorted with the paper recycling at Stanford, so be sure to remember this!"
+ },
+ {
+   "Image": "chopstick holster.png",
+   "bin": "paper",
+   "incorrect": "These will generally be uncontaminated, so put them in the paper bin!"
+ },
+ {
+   "Image": "chopsticks.png",
+   "bin": "compost",
+   "incorrect": "These are food soiled wood, so compost them!"
+ },
+ {
+   "Image": "clean plastic lid.png",
+   "bin": "plastic",
+   "incorrect": "This can only be recycled if it is clean! Make sure to check the type of plastic inside the symbol. "
+ },
+ {
+   "Image": "cling wrap.png",
+   "bin": "paper",
+   "incorrect": "This one is tricky. Clear plastics like cling and bubble wrap are sorted with the paper recycling at Stanford, so be sure to remember this!"
+ },
+ {
+   "Image": "compostable fork.png",
+   "bin": "compost",
+   "incorrect": "Don't throw these into the plastic bin! Only compost them, otherwise you may contaminate the plastic recycling. "
+ },
+ {
+   "Image": "compostable plastic lid.jpg",
+   "bin": "compost",
+   "incorrect": "Remember to check for the compostable symbol. Do not recycle compostable plastics as they are considered to be contaminants of real plastic!"
+ },
+ {
+   "Image": "compostable straw wrapper.png",
+   "bin": "compost",
+   "incorrect": "This can also be thrown in the paper bin if the straw wrapper is clean. "
+ },
+ {
+   "Image": "food soiled paper box.png",
+   "bin": "compost",
+   "incorrect": "These boxes can be composted, it just takes longer than usual. However, they cannot be recycled as they are contaminated with food, so always compost them!"
+ },
+ {
+   "Image": "food soiled paper plate.png",
+   "bin": "compost",
+   "incorrect": "These boxes can be composted, it just takes longer than usual. However, they cannot be recycled as they are contaminated with food, so always compost them!"
+ },
+ {
+   "Image": "food soiled takeout box.png",
+   "bin": "compost",
+   "incorrect": "Anything with leftover food waste (paper or compostable plastic) must be composted! Otherwise, contamination can ruin an entire bin. "
+ },
+ {
+   "Image": "fortune cookie.png",
+   "bin": "compost",
+   "incorrect": "Food is compost!"
+ },
+ {
+   "Image": "panda napkin.png",
+   "bin": "compost",
+   "incorrect": "If this is contaminated with food, only compost it!"
+ },
+ {
+   "Image": "paper donut box.png",
+   "bin": "compost",
+   "incorrect": "These boxes can be composted, it just takes longer than usual. However, they cannot be recycled as they are contaminated with food, so always compost them!"
+ },
+ {
+   "Image": "pizza box.png",
+   "bin": "compost",
+   "incorrect": "These boxes can be composted, it just takes longer than usual. However, they cannot be recycled as they are contaminated with food, so always compost them!"
+ },
+ {
+   "Image": "salt and pepper packs.png",
+   "bin": "compost",
+   "incorrect": "You could also throw this in the paper recycling if the packs are completely empty! If they have leftovers, compost it. "
+ },
+ {
+   "Image": "sauce packets.png",
+   "bin": "landfill",
+   "incorrect": "These materials are not only contaminated but non-recyclable. They can only go to the landfill."
+ },
+ {
+   "Image": "stack of paper.png",
+   "bin": "paper",
+   "incorrect": "The paper bin is your pset cememtery. "
+ },
+ {
+   "Image": "starbucks coffee lid.png",
+   "bin": "compost",
+   "incorrect": "Some Starbucks locations have compostable lids while others do not. Be sure to check for the compostable symbol on the lid to be sure! Otherwise, landfill it. "
+ },
+ {
+   "Image": "starbucks coffee sleeve.png",
+   "bin": "paper",
+   "incorrect": "This is corrugated cardboard, so if you have access to that bin, use it! Otherwise, compost it or put it in the paper recycling. "
+ },
+ {
+   "Image": "starbucks cup.png",
+   "bin": "plastic",
+   "incorrect": "Remember that the cup must be clean/washed out if you place it in the plastic bin! Otherwise, it must be landfilled. "
+ },
+ {
+   "Image": "starbucks pastry bag.png",
+   "bin": "compost",
+   "incorrect": "Food soiled paper is compost!"
+ },
+ {
+   "Image": "starbucks straw.png",
+   "bin": "landfill",
+   "incorrect": "Straws can't be recycled. Try to remove straws from your everyday life, especially if you care about the turtles!"
+ },
+ {
+   "Image": "straw wrapper paper.png",
+   "bin": "paper",
+   "incorrect": "These will generally be uncontaminated, so put them in the paper bin!"
+ },
+ {
+   "Image": "subway straw.png",
+   "bin": "landfill",
+   "incorrect": "Straws can't be recycled. Try to remove straws from your everyday life, especially if you care about the turtles!"
+ },
+ {
+   "Image": "used waxpaper takeout box.png",
+   "bin": "landfill",
+   "incorrect": "Wax paper cannot be recycled because they are made of paper and plastic. Try to go without them, but if you use one, landfill it. "
+ }
+];
